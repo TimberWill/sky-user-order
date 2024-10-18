8e193ac590557776957b89584a4434e87260513d
TimberWill <935257967@qq.com>
2024-10-18 14:06:31 +0800

    初始化-用户端

---
+++ sky-user-common/src/test/java/com/sky/common/SkyUserCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyUserCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ sky-user-server/src/main/java/com/sky/user/SkyUserServerApplication.java
@@ -0,0 +1,13 @@
+package com.sky.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyUserServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkyUserServerApplication.class, args);
+    }
+
+}
